5ed46bc94ec6a7f33acd93d8bbcd0ca2c8592bef
lilizhao <lilizhao1@oneapm.com>
2019-07-11 15:28:56 +0800

    完成动态编译测试部分

--- src/test/java/com/oneapm/compiler/CompilerTest.java
+++ src/test/java/com/oneapm/compiler/CompilerTest.java
@@ -7,15 +7,16 @@ public class CompilerTest {
     public static void main(String[] args) {
         //测试单个文件
         String includePath = null;
+        String thirdJarPath = "/Users/oneapm/.m2/repository/com/alibaba/fastjson/1.2.30/fastjson-1.2.30.jar";
         String classPath = ".";
         File[] files = new File[] {
-                new File("/Users/oneapm/git/dynamic-compile/src/test/java/com/oneapm/compiler/HelloTest.java"),
-                new File("/Users/oneapm/git/dynamic-compile/src/test/java/com/oneapm/compiler/Bean.java"),
+                new File("/Users/oneapm/Desktop/HelloTest.java"),
+                new File("/Users/oneapm/Desktop/Bean.java"),
         };
 
         try {
             Compiler compiler = new Compiler(includePath);
-            classPath += File.pathSeparator + System.getProperty("java.class.path");
+            classPath += File.pathSeparator + System.getProperty("java.class.path") + File.pathSeparator + thirdJarPath;
             Map<String, byte[]> classes = compiler.compile(files, new PrintWriter(System.err), ".", classPath);
 
             classes.forEach((x, y) -> {
--- src/test/java/com/oneapm/compiler/HelloTest.java
+++
@@ -1,9 +0,0 @@
-package com.oneapm.compiler;
-
-public class HelloTest {
-    public static void main(String[] args) {
-        Bean bean = new Bean();
-
-        System.out.println("test: " + bean.getI());
-    }
-}
